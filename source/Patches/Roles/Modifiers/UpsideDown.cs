04d6ad39c80fef464a6b3eabd3aab82b152f2fe2
Xslash <75801324+Xslash58@users.noreply.github.com>
2024-04-25 18:37:14 +0200

    New Modifier: Upside-down

---
+++ source/Patches/Roles/Modifiers/UpsideDown.cs
@@ -0,0 +1,27 @@
+﻿using TownOfUs.Extensions;
+using UnityEngine;
+
+namespace TownOfUs.Roles.Modifiers
+{
+    public class UpsideDown : Modifier, IVisualAlteration
+    {
+        public UpsideDown(PlayerControl player) : base(player)
+        {
+            Name = "Upside-down";
+            TaskText = () => "⅄on ɐɹǝ ndsᴉpǝ poʍu";
+            Color = Patches.Colors.Mini;
+            ModifierType = ModifierEnum.UpsideDown;
+        }
+
+        public bool TryGetModifiedAppearance(out VisualAppearance appearance)
+        {
+            appearance = Player.GetDefaultAppearance();
+            appearance.SizeFactor = new Vector3(appearance.SizeFactor.x, -appearance.SizeFactor.y, appearance.SizeFactor.z);
+
+            if (Player == PlayerControl.LocalPlayer)
+                Camera.main.transform.rotation = Quaternion.Euler(0, 0, 180);
+            
+            return true;
+        }
+    }
+}
