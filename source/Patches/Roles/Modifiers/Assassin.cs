2d61d3be9a3c87cc7954306ffb2574daa1113ab0
Xslash <75801324+Xslash58@users.noreply.github.com>
2024-06-16 18:37:57 +0200

    Properly name freeze

--- source/Patches/Roles/Modifiers/Assassin.cs
+++ source/Patches/Roles/Modifiers/Assassin.cs
@@ -104,7 +104,7 @@ namespace TownOfUs.Roles.Modifiers
             if (CustomGameOptions.AssassinCrewmateGuess) ColorMapping.Add("Crewmate", Colors.Crewmate);
             //Add modifiers if enabled
             if (CustomGameOptions.AssassinGuessModifiers && CustomGameOptions.BaitOn > 0) ColorMapping.Add("Bait", Colors.Bait);
-            if (CustomGameOptions.AssassinGuessModifiers && CustomGameOptions.FreezeOn > 0) ColorMapping.Add("Bait", Colors.Bait);
+            if (CustomGameOptions.AssassinGuessModifiers && CustomGameOptions.FreezeOn > 0) ColorMapping.Add("Freeze", Colors.Bait);
             if (CustomGameOptions.AssassinGuessModifiers && CustomGameOptions.AftermathOn > 0) ColorMapping.Add("Aftermath", Colors.Aftermath);
             if (CustomGameOptions.AssassinGuessModifiers && CustomGameOptions.DiseasedOn > 0) ColorMapping.Add("Diseased", Colors.Diseased);
             if (CustomGameOptions.AssassinGuessModifiers && CustomGameOptions.FrostyOn > 0) ColorMapping.Add("Frosty", Colors.Frosty);
